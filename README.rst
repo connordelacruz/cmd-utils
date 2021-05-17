cd00dc8387ecd3325f5fa30d71293824a66efd28
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-05-17 14:43:21 -0500

    Added url, license, and classifiers

--- README.rst
+++ README.rst
@@ -2,5 +2,7 @@
 Command Line Utilities
 ======================
 
+.. todo badges
+
 Various handy helper functions for printing and input prompting for command line tools.
 
--- setup.py
+++ setup.py
@@ -8,14 +8,30 @@ with open(os.path.join(base_path, 'README.rst')) as f:
 
 setup(
     name='cmd_utils',
-    version='0.1.0',
+    version='0.1.0', # TODO test, then 1.0?
     author='Connor de la Cruz',
     author_email='connor.c.delacruz@gmail.com',
     description='Utilities for command line tools.',
     long_description=readme,
+    url='https://github.com/connordelacruz/cmd-utils',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Topic :: Software Development :: Version Control :: Git',
+        'Topic :: Software Development',
+        'Development Status :: 4 - Beta',
+    ],
     packages=find_packages(),
     install_requires=[
         'blessings>=1.7,<1.8',
     ],
-    # TODO: license, url, classifiers
 )
