5c18971b3a703c45198a393212e9c2fe20f0cf97
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-05-17 14:50:45 -0500

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(base_path, 'README.rst')) as f:
 
 setup(
     name='cmd_utils',
-    version='0.1.0', # TODO test, then 1.0?
+    version='1.0.0',
     author='Connor de la Cruz',
     author_email='connor.c.delacruz@gmail.com',
     description='Utilities for command line tools.',
@@ -28,7 +28,7 @@ setup(
         'Environment :: Console :: Curses',
         'Topic :: Software Development :: Version Control :: Git',
         'Topic :: Software Development',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
     ],
     packages=find_packages(),
     install_requires=[
